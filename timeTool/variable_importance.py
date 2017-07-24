497a0abcb0c1232cdd87a7770f8934e134709c65
Katherine A Fotion <kfotion@psanaphi104.pcdsn>
2017-07-24 15:00:27 -0700

    Parallelized variable importance calculation

--- timeTool/variable_importance.py
+++ timeTool/variable_importance.py
@@ -6,57 +6,60 @@ from random import randint
 import numpy as np
 import extract_row as er
 import os
+from multiprocessing import Pool
+from tqdm import tqdm
+from functools import partial
 
 # Read all shots from a run
-def access_data(data_dir, run_num):
+def access_signals():
+	global signals, num_shots
+	
 	with open(data_dir + 'xppl3816_r' + str(run_num) + '_matrix.dat', 'r') as f:
-                lines = f.readlines()
-        signals = np.matrix([[float(x) for x in line.strip('\n').split(' ')] for line in lines if not line.startswith('#')])
+		signals = np.matrix([[float(x) for x in line.strip('\n').split(' ')] for i,line in tqdm(enumerate(f)) if not line.startswith('#')])	
 
-	return signals
-
+	num_shots = i
+	
 # Calculate variable importance for every feature
-def calculate_vi(model, signals, shot_num):
+def calculate_vi(original_signal, original_pred):
 
-        # Access original signal and perform regression on it 
-        signal = signals[shot_num]
-        original_pred = model.predict(signal.reshape(1, -1))
+	# Prep for multiprocessing
+	pool = Pool()
 
-        # Track max difference in prediction and the corresponding index
-        pred_diff = {}
+	pred_diff = pool.map(partial(iterate_features, original_signal, original_pred), range(original_signal.shape[1]))
 
-        # For each feature, replace it with the feature from a randomly selected data point 3 times
-        for i in range(0, signal.shape[1]):
+	pool.close()
 
-		pred_diff_sum = 0.0
+        return pred_diff
 
-		# Permute 3 times for each feature and average for more precise answer
-		for j in range(0,3):
-                
-			# Access original signal then replace feature value with randomly selected shot
-			permuted_signal = signal
-                	permuted_signal.put(i, signals[randint(0,signals.shape[0]-1)].item(i))
+def iterate_features(original_signal, original_pred, i):
+	
+	pred_diff_sum = 0.0
 
-			# Calculate difference in prediction with permutation
-                	new_pred = model.predict(permuted_signal.reshape(1, -1))
-                	pred_diff_sum += math.fabs(original_pred - new_pred)
-		
-		# Average 3 instances
-		pred_diff[i] = pred_diff_sum / 3.0
+        # Permute 3 times for each feature and average for more precise answer
+        for j in range(0,3):
 
-        return list(pred_diff.values())
+        	# Access original signal then replace feature value with randomly selected shot
+		permuted_signal = original_signal
+		rand_signal = signals[randint(0,num_shots-1)]
+		permuted_signal.put(i, rand_signal.item(i))
+
+                # Calculate difference in prediction with permutation
+		new_pred = model.predict(permuted_signal.reshape(1, -1))
+		pred_diff_sum += math.fabs(original_pred - new_pred)
+
+                # Average 3 instances
+		pred_d = pred_diff_sum / 3.0
+
+	return pred_d
 
 # Write file containing variable importance of each feature to file
-def write_vi(write_dir, run_num, shot_num, pred_diff):
-
+def write_vi(write_dir, shot_num, pred_diff):
 	with open(write_dir + 'vi/vi_data/r' + str(run_num) + '_s' + str(shot_num) + '_vi.dat','w') as f:
 		f.writelines([str(diff) + '\n' for diff in pred_diff])
 
 # Write gnuplot file to display results overlaid on signal
-def write_gnuplot(run_num, shot_num, write_dir):
-
+def write_gnuplot(write_dir, shot_num):
 	with open(write_dir + 'vi/vi.gp','w') as f:
-
 		f.write('set term png\n')
 		f.write('set output \'../work/transferLearning/pngs_to_label/r' + str(run_num) + '_s' + str(shot_num) + '_vi.png\'\n')
 		f.write('set xlabel \'index\'\n')
@@ -68,8 +71,12 @@ def write_gnuplot(run_num, shot_num, write_dir):
 	# Run gnuplot script to generate png
 	os.system('gnuplot ' + write_dir + 'vi/vi.gp')
 
-def main(run_num, shot_num, data_dir, write_dir, inc, est):
-
+def main(run, shot_nums, d_dir, write_dir, est):
+	global data_dir, run_num, model
+	
+	data_dir = d_dir
+	run_num = run
+	
 	# Access regressor that was selected
         if est == 'RF':
                 estimator_type = randomForestRegressor
@@ -80,30 +87,35 @@ def main(run_num, shot_num, data_dir, write_dir, inc, est):
         [inputs, ground_truths, num_samples, num_features] = estimator_type.read_training_data(data_dir, write_dir, run_num)
 	
 	# Train forest on training data
-	forest = estimator_type.train(inputs, ground_truths)
+	model = estimator_type.train(inputs, ground_truths)
 
-	# Access all data 
-	signals = access_data(data_dir, run_num)
-
-	# Inc is increment of shots (i.e. every 100 shots)
-	if inc is None:
-		shot_nums = [shot_num]
-	else:
-		shot_nums = [i for i in range(0,signals.shape[0],inc)]
-
+	# Access all shots
+	print('Reading data file')
+	access_signals()
+	
 	# Loop through all specified shot numbers
-	for shot_num in shot_nums:
+	pool = Pool()
+	
+	print('Calculating variable importances')
+	for shot in tqdm(shot_nums.split(' ')):
+	
+		shot_num = int(float(shot))
+
+		# Access original signal and perform regression on it
+		original_signal = signals[shot_num] 
+       		original_pred = model.predict(original_signal.reshape(1, -1))
+
 		# Calculate variable importance for all features
-		importances = calculate_vi(forest, signals, shot_num)
+		importances = calculate_vi(original_signal, original_pred)
 
 		# Write vi results to file
-		write_vi(write_dir, run_num, shot_num, importances)
+		write_vi(write_dir, shot_num, importances)
 
 		# Write shot row data to column format for gnuplot
-                er.write_row_to_col(run_num, shot_num, data_dir, write_dir)
+        	er.write_row_to_col(run_num, shot_num, data_dir, write_dir)
 
 		# Write gnuplot file
-		write_gnuplot(run_num, shot_num, write_dir)		
+		write_gnuplot(write_dir, shot_num)		
 
 if __name__=="__main__": 
 
@@ -111,7 +123,7 @@ if __name__=="__main__":
         helpstr = 'Determine most import variable in a particular data point'
         parser = argparse.ArgumentParser(description=helpstr);
         parser.add_argument('-r','--run', dest='run', type=int, help='run number', default=52)
-	parser.add_argument('-s','--shot',dest='shot', type=int, help='shot number', default=0)
+	parser.add_argument('-s','--shots',dest='shots', type=str, help='shot number(s)', default='0')
 	parser.add_argument('-i','--inc', dest='increment', type=int, help='every __ shots', default=None)
 	parser.add_argument('-d','--directory',dest='directory', type=str, help='path to directory with original data files', default='/reg/d/psdm/XPP/xppl3816/scratch/timeTool_ml/data_source/')
         parser.add_argument('-w','--write',dest='write',type=str, help='path to scratch directory where files are  written to', default='/reg/d/psdm/XPP/xppl3816/scratch/timeTool_ml/data_results/')
@@ -121,12 +133,11 @@ if __name__=="__main__":
 
         # Access input
         run_num = args.run
-	shot_num = args.shot
+	shot_nums = args.shots
 	data_dir = args.directory
 	write_dir = args.write
-	inc = args.increment
 	est = args.model
 
         # Call main()
-        main(run_num, shot_num, data_dir, write_dir, inc, est)
+        main(run_num, shot_nums, data_dir, write_dir, est)
 
